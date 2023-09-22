d6e3c5c294e0e28a9d2032025fed19516c19ba7b
joshimps <joshimpey10@gmail.com>
2023-09-22 12:14:52 +1000

    Working on class structure

--- matlab/controller.m
+++ matlab/controller.m
@@ -11,15 +11,21 @@ classdef controller < handle
    methods (Access=public)
       
       % -------------------------------------------------------------------------- %
-      %                                Constructors                                 %
+      %                                Constructor                                 %
       % -------------------------------------------------------------------------- %
       
-      
-      function self = Controller()
+      function self = Controller(uR3BaseTransform)
          hold on
          run('Peter Cork Toolbox\rvctools\startup_rvc.m')
          self.logger = log4matlab('log.log'); 
-         self.logger.SetCommandWindowLevel(self.logger.DEBUG) 
+         self.logger.SetCommandWindowLevel(self.logger.DEBUG)
+
+         
+         self.UR3Object = UR3(uR3BaseTransform);
+         self.logger.mlog = {self.logger.DEBUG,'LinearUR3Controller',"Linear UR3 Created With Base Transform:"};
+         self.logger.mlog = {self.logger.DEBUG,'LinearUR3Controller',self.logger.MatrixToString(self.robotObject.model.base.T)}; 
+        
+        
       end
       
       % -------------------------------------------------------------------------- %
