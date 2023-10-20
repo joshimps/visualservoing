a59cb0df3e38ba7deaf46dc3a2bd9aadf8eaa9e0
JoshEmbedded <joshua.schiff.2000@gmail.com>
2023-10-20 19:13:27 +1100

    fixed synchronization

--- src/aruco_rgbd.py
+++ src/aruco_rgbd.py
@@ -15,6 +15,7 @@ ANGLE_INC_V = VFOV/HEIGHT
 MIN_ANGLE_V = -(VFOV/2)
 
 def process_depth(depth, pixel, pose):
+    print('foo')
     u = pixel.point.x
     theta = MIN_ANGLE_H - ANGLE_INC_H * u
     v = pixel.point.y
@@ -39,12 +40,12 @@ if __name__ == "__main__":
     rospy.init_node("Aruco_RGBD")
     rospy.loginfo("Starting Aruco_RGBD.")
     depth_sub = message_filters.Subscriber("/camera/aligned_depth_to_color/image_raw", Image)
-    pixel_sub = message_filters.Subscriber("/aruco_singe/pixel", PointStamped)
+    pixel_sub = message_filters.Subscriber("/aruco_single/pixel", PointStamped)
     pose_sub = message_filters.Subscriber("/aruco_single/pose", PoseStamped)
 
     rgbd_pub = rospy.Publisher("/aruco_single/pose_rgbd", PoseStamped, queue_size=5)
     
-    sync_sub = message_filters.ApproximateTimeSynchronizer([depth_sub,pixel_sub, pose_sub], 5, 0.2)
+    sync_sub = message_filters.TimeSynchronizer([depth_sub,pixel_sub, pose_sub], 100)
     sync_sub.registerCallback(process_depth)    
     while not rospy.is_shutdown():
         pass
