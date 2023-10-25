89eecb6eda251a12f60df5cb417b43fb609a19f9
sheepskins <mitchrosser13@gmail.com>
2023-10-25 21:05:01 +1100

    Code Clean, readme updat, rgbd handheld servo done

--- src/aruco_rgbd.py
+++ src/aruco_rgbd.py
@@ -4,36 +4,27 @@ import message_filters
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import PointStamped, PoseStamped
 from math import sin, cos
+from cv_bridge import CvBridge
 
-HEIGHT = 480
-WIDTH = 640
-VFOV = 42
-HFOV = 69
-ANGLE_INC_H = HFOV/WIDTH  
-MIN_ANGLE_H = (HFOV/2)
-ANGLE_INC_V = VFOV/HEIGHT  
-MIN_ANGLE_V = -(VFOV/2)
+bridge = CvBridge()
 
 def process_depth(depth, pixel, pose):
-    print('foo')
-    u = pixel.point.x
-    theta = MIN_ANGLE_H - ANGLE_INC_H * u
-    v = pixel.point.y
-    gamma = MIN_ANGLE_V + ANGLE_INC_V * v
-    index = u * depth.step + v
-    marker_depth = depth.data[index]
-    x = marker_depth * sin(theta)
-    y = marker_depth * sin(gamma)
-    z = marker_depth * cos(gamma)
+    # Extract Pixels
+    u = int(round(pixel.point.x))
+    v = int(round(pixel.point.y))
+    # Convert depth image to mm
+    depthCV = bridge.imgmsg_to_cv2(depth, desired_encoding="passthrough")
+    # Extract depth value from depth image in metres
+    marker_depth = depthCV[v,u]/1000
     
+    # Use P3P algorithm from aruco ros node to determine orientation, x and y values
+    # replace z value with depth from depth image
     msg = PoseStamped()
     msg.header = pose.header
     msg.pose.orientation = pose.pose.orientation
-    msg.pose.position.x = x
-    msg.pose.position.y = y
-    msg.pose.position.z = z
+    msg.pose.position = pose.pose.position
+    msg.pose.position.x = marker_depth
     rgbd_pub.publish(msg)
-
 
 
 if __name__ == "__main__":
@@ -44,8 +35,8 @@ if __name__ == "__main__":
     pose_sub = message_filters.Subscriber("/aruco_single/pose", PoseStamped)
 
     rgbd_pub = rospy.Publisher("/aruco_single/pose_rgbd", PoseStamped, queue_size=5)
-    
-    sync_sub = message_filters.TimeSynchronizer([depth_sub,pixel_sub, pose_sub], 100)
+    # Subscribe to depth image, pixel centre and pose of detected marker
+    sync_sub = message_filters.ApproximateTimeSynchronizer([depth_sub,pixel_sub, pose_sub], 3, 0.1)
     sync_sub.registerCallback(process_depth)    
     while not rospy.is_shutdown():
         pass
