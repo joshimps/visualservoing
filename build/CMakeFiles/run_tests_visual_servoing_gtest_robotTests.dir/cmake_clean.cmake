0fcff40af403148d0c56bcbcc0cd380eb50aabb7
joshimps <102491126+joshimps@users.noreply.github.com>
2023-10-30 11:30:37 +1100

    Delete build directory

--- build/CMakeFiles/run_tests_visual_servoing_gtest_robotTests.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_tests_visual_servoing_gtest_robotTests"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_tests_visual_servoing_gtest_robotTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
